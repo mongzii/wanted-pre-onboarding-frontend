ff237a2a33d0515aeaa2fb9e293a8e8c77e4c8f2
moonjiyoung <umarg228@naver.com>
2023-06-13 15:30:03 +0900

    feat: 페이지 기본셋팅 홈까지완료

--- src/App.js
+++ src/App.js
@@ -1,7 +1,24 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 
+import Home from "./Page/Home";
+// import Login from "./Page/Login";
+// import SignUp from "./Page/SignUp";
+// import Todo from "./Page/Todo";
+
 function App() {
-  return <div className="App">dfdf</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          {/* <Route path="/login" element={<Login />}></Route>
+          <Route path="/signup" element={<SignUp />}></Route>
+          <Route path="/todo" element={<Todo />}></Route> */}
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
---
+++ src/Page/Home.js
@@ -0,0 +1,15 @@
+import styled from "styled-components";
+
+const Container = styled.div`
+  color: blue;
+`;
+
+function Home() {
+  return (
+    <div>
+      <Container>홈입니다</Container>
+    </div>
+  );
+}
+
+export default Home;
